7545b3e71ed3e9a1aef9b61b06cf1c0350a02476
Markus Plutka <markus.plutka@de.hellmann.net>
2016-04-16 00:58:29 +0200

    Fixed add to cart from wishlist

---
+++ app/code/community/OrganicInternet/SimpleConfigurableProducts/controllers/Wishlist/IndexController.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Wishlist
+ * @copyright  Copyright (c) 2006-2016 X.commerce, Inc. and affiliates (http://www.magento.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+
+/**
+ * Wishlist front controller
+ *
+ * @category    Mage
+ * @package     Mage_Wishlist
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+require_once 'Mage/Wishlist/controllers/IndexController.php';
+class OrganicInternet_SimpleConfigurableProducts_Wishlist_IndexController extends Mage_Wishlist_IndexController
+{
+    /**
+     * Add wishlist item to shopping cart and remove from wishlist
+     *
+     * If Product has required options - item removed from wishlist and redirect
+     * to product view page with message about needed defined required options
+     */
+    public function cartAction()
+    {
+        if (!$this->_validateFormKey()) {
+            return $this->_redirect('*/*');
+        }
+        $itemId = (int) $this->getRequest()->getParam('item');
+
+        /* @var $item Mage_Wishlist_Model_Item */
+        $item = Mage::getModel('wishlist/item')->load($itemId);
+        if (!$item->getId()) {
+            return $this->_redirect('*/*');
+        }
+        $wishlist = $this->_getWishlist($item->getWishlistId());
+        if (!$wishlist) {
+            return $this->_redirect('*/*');
+        }
+
+        // Set qty
+        $qty = $this->getRequest()->getParam('qty');
+        if (is_array($qty)) {
+            if (isset($qty[$itemId])) {
+                $qty = $qty[$itemId];
+            } else {
+                $qty = 1;
+            }
+        }
+        $qty = $this->_processLocalizedQty($qty);
+        if ($qty) {
+            $item->setQty($qty);
+        }
+
+        /* @var $session Mage_Wishlist_Model_Session */
+        $session    = Mage::getSingleton('wishlist/session');
+        $cart       = Mage::getSingleton('checkout/cart');
+
+        $redirectUrl = Mage::getUrl('*/*');
+
+        try {
+            $options = Mage::getModel('wishlist/item_option')->getCollection()
+                    ->addItemFilter(array($itemId));
+            $item->setOptions($options->getOptionsByItem($itemId));
+            
+            $itemBuyRequest = $item->getBuyRequest();
+            $childProduct = null;
+            if($itemBuyRequest['super_attribute']) {
+               $childProduct = $item->getProduct()->getTypeInstance(true)->getProductByAttributes($itemBuyRequest['super_attribute'], $item->getProduct());
+               $childProduct = Mage::getModel('catalog/product')->load($childProduct->getId());
+
+               $itemBuyRequest->setData('product', $childProduct->getId());
+            }            
+
+            $buyRequest = Mage::helper('catalog/product')->addParamsToBuyRequest(
+                $this->getRequest()->getParams(),
+                array('current_config' => $itemBuyRequest)
+            );
+            
+            // Overwrite stored product with loaded simple configurable because it holds stock info
+            if($childProduct) {
+                $buyRequest->setData('product', $childProduct->getId()); 
+                $item->setProduct($childProduct);                
+            }
+            $item->mergeBuyRequest($buyRequest);
+           
+            if ($item->addToCart($cart, true)) {
+                $cart->save()->getQuote()->collectTotals();
+            }
+
+            $wishlist->save();
+            Mage::helper('wishlist')->calculate();
+
+            if (Mage::helper('checkout/cart')->getShouldRedirectToCart()) {
+                $redirectUrl = Mage::helper('checkout/cart')->getCartUrl();
+            }
+            Mage::helper('wishlist')->calculate();
+
+            $product = Mage::getModel('catalog/product')
+                ->setStoreId(Mage::app()->getStore()->getId())
+                ->load($item->getProductId());
+            $productName = Mage::helper('core')->escapeHtml($product->getName());
+            $message = $this->__('%s was added to your shopping cart.', $productName);
+            Mage::getSingleton('catalog/session')->addSuccess($message);
+        } catch (Mage_Core_Exception $e) {
+            if ($e->getCode() == Mage_Wishlist_Model_Item::EXCEPTION_CODE_NOT_SALABLE) {
+                $session->addError($this->__('This product(s) is currently out of stock'));
+            } else if ($e->getCode() == Mage_Wishlist_Model_Item::EXCEPTION_CODE_HAS_REQUIRED_OPTIONS) {
+                Mage::getSingleton('catalog/session')->addNotice($e->getMessage());
+                $redirectUrl = Mage::getUrl('*/*/configure/', array('id' => $item->getId()));
+            } else {
+                Mage::getSingleton('catalog/session')->addNotice($e->getMessage());
+                $redirectUrl = Mage::getUrl('*/*/configure/', array('id' => $item->getId()));
+            }
+        } catch (Exception $e) {
+            Mage::logException($e);
+            $session->addException($e, $this->__('Cannot add item to shopping cart'));
+        }
+
+        Mage::helper('wishlist')->calculate();
+
+        return $this->_redirectUrl($redirectUrl);
+    }
+}
